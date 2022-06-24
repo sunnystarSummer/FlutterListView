5b3c74ffd9cd727dbea597e8ac9eab875d34a97d
JasonWiseWhale <jason.wang@wise-whale.com.tw>
2022-06-24 17:10:08 +0800

    新增navigator

---
+++ flutter_list_view/lib/navigator/example02.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+//https://medium.com/flutter/learning-flutters-new-navigation-and-routing-system-7c9068155ade
+
+void main() {
+  runApp(Nav2App());
+}
+
+class Nav2App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/details': (context) => DetailScreen(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: FlatButton(
+          child: Text('View Details'),
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              '/details',
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: FlatButton(
+          child: Text('Pop!'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
